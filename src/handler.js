0c07e69c2526aa825f79c2203b9944b07c145b2e
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-06-07 20:24:58 +0100

    feat(routes): create `/discord/contributors` to prevent having to manually update json

--- src/handler.js
+++ src/handler.js
@@ -5,6 +5,7 @@ import { getGames, getGameId, getAsset } from "./routes/games.js";
 import { getGenerators, getGeneratorGameId } from "./routes/ocGenerators.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import { responseHeaders } from "./lib/responseHeaders.js";
+import { getContributors } from "./routes/discord.js";
 import { index } from "./routes/index.js";
 
 const router = Router();
@@ -12,17 +13,25 @@ const router = Router();
 router
     .get("/", errorHandler(index))
     .get("/games", errorHandler(getGames))
-    .get("/game/:gameId",  errorHandler(getGameId))
-    .get("/game/:gameId/:asset",  errorHandler(getAsset))
-    .get("/oc-generators",  errorHandler(getGenerators))
-    .get("/oc-generator/:gameId",  errorHandler(getGeneratorGameId))
-    .all("*", () => new Response(JSON.stringify({
-        success: false,
-        status: "error",
-        error: "404 Not Found",
-    }), {
-        headers: responseHeaders,
-    }));
+    .get("/game/:gameId", errorHandler(getGameId))
+    .get("/game/:gameId/:asset", errorHandler(getAsset))
+    .get("/oc-generators", errorHandler(getGenerators))
+    .get("/oc-generator/:gameId", errorHandler(getGeneratorGameId))
+    .get("/discord/contributors", errorHandler(getContributors))
+    .all(
+        "*",
+        () =>
+            new Response(
+                JSON.stringify({
+                    success: false,
+                    status: "error",
+                    error: "404 Not Found",
+                }),
+                {
+                    headers: responseHeaders,
+                }
+            )
+    );
 
 addEventListener("fetch", (event) => {
     event.respondWith(router.handle(event.request));
---
+++ src/routes/discord.js
@@ -0,0 +1,64 @@
+import { responseHeaders } from "../lib/responseHeaders.js";
+import { roles, guildId } from "../lib/discord.js";
+
+export const getContributors = async (request, env) => {
+    const members = [];
+
+    let after = null;
+
+    while (true) {
+        const response = await fetch(
+            `https://discord.com/api/guilds/${guildId}/members?limit=1000${
+                after ? `&after=${after}` : ""
+            }`,
+            {
+                headers: {
+                    Authorization: `Bot ${env.DISCORD_TOKEN}`,
+                },
+            }
+        );
+
+        const guildMembers = await response.json();
+
+        const filteredMembers = guildMembers.filter((member) => {
+            return member.roles.some((role) =>
+                Object.keys(roles).includes(role)
+            );
+        });
+
+        const contributors = filteredMembers.map((member) => {
+            const rolesArray = member.roles
+                .map((role) => roles[role])
+                .filter((role) => role);
+            return {
+                id: member.user.id,
+                username: member.user.username,
+                avatar: `https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.webp`,
+                roles: rolesArray,
+            };
+        });
+
+        members.push(...contributors);
+
+        if (
+            !guildMembers.length ||
+            !guildMembers[guildMembers.length - 1].user
+        ) {
+            break;
+        }
+
+        after = guildMembers[guildMembers.length - 1].user.id;
+    }
+
+    return new Response(
+        JSON.stringify({
+            success: true,
+            status: "ok",
+            path: `/discord/contributors`,
+            contributors: members,
+        }),
+        {
+            headers: responseHeaders,
+        }
+    );
+};
