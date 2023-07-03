ae089c07c4fb938f93c6340178c43eaf5ff186b6
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-07-03 19:41:55 +0100

    refactor: split search function

---
+++ src/lib/query.ts
@@ -0,0 +1,103 @@
+import type { Asset } from "@/lib/types/asset";
+
+export const getSearchResults = async (
+    query: string,
+    game: string[],
+    asset: string[],
+    tags: string[],
+    env: Env
+): Promise<Asset[]> => {
+    let sqlQuery = `SELECT * FROM assets WHERE 1=1`;
+    const parameters = [];
+
+    sqlQuery = addQueryToSqlQuery(query, sqlQuery, parameters);
+    sqlQuery = addGameToSqlQuery(game, sqlQuery, parameters);
+    sqlQuery = addAssetToSqlQuery(asset, sqlQuery, parameters);
+    sqlQuery = addTagsToSqlQuery(tags, sqlQuery, parameters);
+
+    sqlQuery += ` ORDER BY uploadedDate DESC`;
+
+    sqlQuery = limitResults(sqlQuery, game, asset, tags);
+
+    if (!query && !game.length && !asset.length && !tags.length) {
+        sqlQuery = `SELECT * FROM assets ORDER BY uploadedDate DESC LIMIT 30`;
+    }
+
+    const row: D1Result<Asset> = await env.database
+        .prepare(sqlQuery)
+        .bind(...parameters)
+        .run();
+
+    return row.results.map((result) => ({
+        id: result.id,
+        name: result.name,
+        game: result.game,
+        asset: result.asset,
+        tags: result.tags,
+        url: result.url,
+        verified: result.verified,
+        uploadedBy: result.uploadedBy,
+        uploadedDate: result.uploadedDate,
+        fileSize: result.fileSize,
+    }));
+};
+
+const addQueryToSqlQuery = (
+    query: string,
+    sqlQuery: string,
+    parameters: any[]
+): string => {
+    if (query) {
+        sqlQuery += ` AND name LIKE ?`;
+        parameters.push(`%${query}%`);
+    }
+    return sqlQuery;
+};
+
+const addGameToSqlQuery = (
+    game: string[],
+    sqlQuery: string,
+    parameters: any[]
+): string => {
+    if (game.length) {
+        sqlQuery += ` AND game IN (${game.map(() => "?").join(",")})`;
+        parameters.push(...game);
+    }
+    return sqlQuery;
+};
+
+const addAssetToSqlQuery = (
+    asset: string[],
+    sqlQuery: string,
+    parameters: any[]
+): string => {
+    if (asset.length) {
+        sqlQuery += ` AND asset IN (${asset.map(() => "?").join(",")})`;
+        parameters.push(...asset);
+    }
+    return sqlQuery;
+};
+
+const addTagsToSqlQuery = (
+    tags: string[],
+    sqlQuery: string,
+    parameters: any[]
+): string => {
+    if (tags.length) {
+        sqlQuery += ` AND tags IN (${tags.map(() => "?").join(",")})`;
+        parameters.push(...tags);
+    }
+    return sqlQuery;
+};
+
+const limitResults = (
+    sqlQuery: string,
+    game: string[],
+    asset: string[],
+    tags: string[]
+): string => {
+    // if (game.length > 1 || asset.length > 1 || tags.length > 1) {
+    //     sqlQuery += ` LIMIT 2500`;
+    // }
+    return (sqlQuery += ` LIMIT 2500`);
+};
--- src/routes/search/search.ts
+++ src/routes/search/search.ts
@@ -1,5 +1,6 @@
 import { responseHeaders } from "@/lib/responseHeaders";
 import type { Asset } from "@/lib/types/asset";
+import * as queryLib from "@/lib/query";
 
 export const getSearch = async (
     request: Request,
@@ -10,70 +11,16 @@ export const getSearch = async (
     const game = url.searchParams.get("game")?.split(",") || [];
     const asset = url.searchParams.get("asset")?.split(",") || [];
     const tags = url.searchParams.get("tags")?.split(",") || [];
-    let results: Asset[] = [];
 
-    const cacheKey = new Request(url.toString(), request);
-    const cache = caches.default;
-    let response = await cache.match(cacheKey);
+    const results = await queryLib.getSearchResults(
+        query,
+        game,
+        asset,
+        tags,
+        env
+    );
 
-    if (response) {
-        return response;
-    }
-
-    const parameters = [];
-    let sqlQuery = `SELECT * FROM assets WHERE 1=1`;
-
-    if (query) {
-        sqlQuery += ` AND name LIKE ?`;
-        parameters.push(`%${query}%`);
-    }
-
-    if (game.length) {
-        sqlQuery += ` AND game IN (${game.map(() => "?").join(",")})`;
-        parameters.push(...game);
-    }
-
-    if (asset.length) {
-        sqlQuery += ` AND asset IN (${asset.map(() => "?").join(",")})`;
-        parameters.push(...asset);
-    }
-
-    sqlQuery += tags.length
-        ? ` AND tags IN (${tags.map(() => "?").join(",")})`
-        : "";
-    tags.length && parameters.push(...tags);
-
-    sqlQuery += ` ORDER BY uploadedDate DESC`;
-
-    // if there are more than 1 game or asset, limited to 2500 results to prevent abuse of the API
-    if (game.length > 1 || asset.length > 1) {
-        sqlQuery += ` LIMIT 2500`;
-    }
-
-    // if no parameters are entered, return 30 most recent results instead of all results
-    if (!query && !game.length && !asset.length && !tags.length) {
-        sqlQuery = `SELECT * FROM assets ORDER BY uploadedDate DESC LIMIT 30`;
-    }
-
-    const row: D1Result<Asset> = await env.database
-        .prepare(sqlQuery)
-        .bind(...parameters)
-        .run();
-
-    results = row.results.map((result) => ({
-        id: result.id,
-        name: result.name,
-        game: result.game,
-        asset: result.asset,
-        tags: result.tags,
-        url: result.url,
-        verified: result.verified,
-        uploadedBy: result.uploadedBy,
-        uploadedDate: result.uploadedDate,
-        fileSize: result.fileSize,
-    }));
-
-    response = new Response(
+    const response = new Response(
         JSON.stringify({
             success: true,
             status: "ok",
