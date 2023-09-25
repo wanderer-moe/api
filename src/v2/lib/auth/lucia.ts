854f3987e2fec6eb3cda0571a18fbe06bdf99765
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-09-25 01:51:53 +0100

    cloudflare KV session storage

--- src/v2/lib/auth/lucia.ts
+++ src/v2/lib/auth/lucia.ts
@@ -2,26 +2,46 @@ import { lucia } from "lucia"
 import { hono } from "lucia/middleware"
 import { getConnection } from "@/v2/db/turso"
 import { tableNames } from "@/v2/db/drizzle"
+import { unstorage } from "@lucia-auth/adapter-session-unstorage"
 import { libsql } from "@lucia-auth/adapter-sqlite"
+import { createStorage } from "unstorage"
+import cloudflareKVBindingDriver from "unstorage/drivers/cloudflare-kv-binding"
 import { discord } from "@lucia-auth/oauth/providers"
 
 /**
- * The `auth` function is used to create a `lucia` instance with authentication middleware and a `libsql` adapter.
- * @param env - The environment variables used to configure the authentication middleware and adapter.
+ * Creates a KV session storage using the Cloudflare KV binding driver.
+ * @param {Bindings} env - Hono environment bindings.
+ * @returns The KV session storage.
+ * @see https://unstorage.unjs.io/drivers/cloudflare-kv-binding
+ */
+export function KVSessionStorage(env: Bindings) {
+    return createStorage({
+        driver: cloudflareKVBindingDriver({
+            binding: env.kv,
+        }),
+    })
+}
+
+/** The `auth` function is used to create a `lucia` instance with authentication middleware and a `libsql` adapter.
+ * @param {Bindings} env - The environment variables used to configure the authentication middleware and adapter.
  * @returns A `lucia` instance with authentication middleware and a `libsql` adapter.
  */
-export const auth = (env: Bindings) => {
+export function auth(env: Bindings) {
     const db = getConnection(env)
 
+    const storage = KVSessionStorage(env)
     // as lucia doesn't have a adapter for drizzle, we instead create a direct connection to the database using libsql
     const connection = db.turso
 
     return lucia({
-        adapter: libsql(connection, {
-            key: tableNames.authKey,
-            session: tableNames.authSession,
-            user: tableNames.authUser,
-        }),
+        adapter: {
+            user: libsql(connection, {
+                user: tableNames.authUser,
+                key: tableNames.authKey,
+                session: null, // we are handling sessions w/ CF KV
+            }),
+            session: unstorage(storage),
+        },
         middleware: hono(),
         sessionExpiresIn: {
             idlePeriod: 0,
@@ -64,6 +84,12 @@ export const auth = (env: Bindings) => {
 
 export type Auth = ReturnType<typeof auth>
 
+/**
+ * The `discordAuth` function is used to create a `discord` object for use with the `auth` function.
+ * @param {Auth} auth - Auth instance used by Lucia.
+ * @param {Bindings} env - Hono environment bindings.
+ * @returns - Discord Object.
+ */
 export function discordAuth(auth: Auth, env: Bindings) {
     return discord(auth, {
         clientId: env.DISCORD_CLIENT_ID,
