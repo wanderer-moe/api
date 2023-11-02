637b01f44347c2331b13a796fbca3889cb0b5cfe
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-11-02 19:18:23 +0000

    rewrite resend wrapper to use classes

--- src/v2/lib/resend/email.ts
+++ src/v2/lib/resend/email.ts
@@ -1,55 +1,79 @@
-import { ResendWrapper } from "@/v2/lib/resend/wrapper"
+import { Resend, type EmailData } from "@/v2/lib/resend/wrapper"
 
 const emailFrom = "Test <test@test.wanderer.moe>"
 
-const sendEmail = async (emailData, c) => {
+const sendEmail = async (emailData: EmailData, c: APIContext) => {
     try {
-        return await ResendWrapper(c, emailData)
+        const resend = new Resend(c)
+        await resend.sendEmail(emailData)
     } catch (error) {
-        throw new Error("Error sending email.")
+        throw new Error(`[RESEND]: ${error.message}`)
     }
 }
 
-export const sendPasswordResetEmail = async (email, link, username, c) => {
-    const emailData = {
+const createEmailData = (
+    to: string,
+    subject: string,
+    html: string
+): EmailData => {
+    return {
         from: emailFrom,
-        to: email,
-        subject: "Password Reset Request",
-        html: `<strong>Password reset for ${username}</strong><br /><a href="${link}">Click here to reset your password</a>`,
+        to,
+        subject,
+        html,
     }
+}
 
+export const sendPasswordResetEmail = async (
+    email: string,
+    link: string,
+    username: string,
+    c: APIContext
+) => {
+    const emailData = createEmailData(
+        email,
+        "Password Reset Request",
+        `<strong>Password reset for ${username}</strong><br /><a href="${link}">Click here to reset your password</a>`
+    )
     return sendEmail(emailData, c)
 }
 
-export const sendPasswordChangeEmail = async (email, username, c) => {
-    const emailData = {
-        from: emailFrom,
-        to: email,
-        subject: "Password Updated Confirmation",
-        html: `<strong>Your password for ${username} has been updated.</strong><br /> Wasn't you? Contact us at <a href="mailto:support@wanderer.moe">support@wanderer.moe</a>`,
-    }
-
+export const sendPasswordChangeEmail = async (
+    email: string,
+    username: string,
+    c: APIContext
+) => {
+    const emailData = createEmailData(
+        email,
+        "Password Change Request",
+        `<strong>Your password for ${username} has been changed.</strong><br /> Wasn't you? Contact us at <a href="mailto:support@wanderer.moe">support@wanderer.moe</a>`
+    )
     return sendEmail(emailData, c)
 }
 
-export const sendEmailChangeEmail = async (email, username, c) => {
-    const emailData = {
-        from: emailFrom,
-        to: email,
-        subject: "Email Change Request",
-        html: `<strong>Your email address for ${username} has been changed.</strong><br /> Wasn't you? Contact us at <a href="mailto:support@wanderermoe">support@wanderer.moe</a>`,
-    }
-
+export const sendEmailChangeEmail = async (
+    email: string,
+    username: string,
+    c: APIContext
+) => {
+    const emailData = createEmailData(
+        email,
+        "Email Change Request",
+        `<strong>Your email address for ${username} has been changed.</strong><br /> Wasn't you? Contact us at <a href="mailto:support@wanderer.moe">support@wanderer.moe</a>`
+    )
     return sendEmail(emailData, c)
 }
 
-export const sendEmailConfirmationEmail = async (email, link, username, c) => {
-    const emailData = {
-        from: emailFrom,
-        to: email,
-        subject: "Email Confirmation",
-        html: `<strong>Email confirmation for ${username}</strong><br /><a href="${link}">Click here to confirm your email</a>`,
-    }
-
+export const sendEmailConfirmationEmail = async (
+    email: string,
+    link: string,
+    username: string,
+    c: APIContext
+) => {
+    const emailData = createEmailData(
+        email,
+        "Email Confirmation",
+        `<strong>Email confirmation for ${username}</strong><br /><a href="${link}">Click here to confirm your email</a>`
+    )
     return sendEmail(emailData, c)
 }
--- src/v2/lib/resend/wrapper.ts
+++ src/v2/lib/resend/wrapper.ts
@@ -1,38 +1,43 @@
-type EmailData = {
+export type EmailData = {
     to: string
     from: string
     subject: string
     html: string
 }
 
-export async function ResendWrapper(
-    c: APIContext,
-    emailData: EmailData
-): Promise<Response> {
-    const response = await fetch(`https://api.resend.com/emails`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${c.env.RESEND_API_KEY}`,
-        },
-        body: JSON.stringify({ ...emailData }),
-    })
+export class Resend {
+    private context: APIContext
 
-    const contentType = response.headers.get("content-type") || ""
+    constructor(context: APIContext) {
+        this.context = context
+    }
 
-    if (contentType.includes("application/json")) {
-        const responseBody = JSON.stringify(await response.json())
-        return new Response(responseBody, {
+    async sendEmail(emailData: EmailData): Promise<Response> {
+        const response = await fetch(`https://api.resend.com/emails`, {
+            method: "POST",
             headers: {
-                "content-type": "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${this.context.env.RESEND_API_KEY}`,
             },
+            body: JSON.stringify({ ...emailData }),
         })
-    } else {
-        const responseBody = await response.text()
-        return new Response(responseBody, {
-            headers: {
-                "content-type": contentType,
-            },
-        })
+
+        const contentType = response.headers.get("content-type") || ""
+
+        if (contentType.includes("application/json")) {
+            const responseBody = JSON.stringify(await response.json())
+            return new Response(responseBody, {
+                headers: {
+                    "content-type": "application/json",
+                },
+            })
+        } else {
+            const responseBody = await response.text()
+            return new Response(responseBody, {
+                headers: {
+                    "content-type": contentType,
+                },
+            })
+        }
     }
 }
