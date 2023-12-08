eadf0ea1bef05146e82c48d9572d17acf718fe6e
dromzeh <65048232+dromzeh@users.noreply.github.com>
2023-12-08 14:30:01 +0000

    migrate lucia config to v3

---
+++ src/v2/lib/auth/definitions/auth-definitions.ts
@@ -0,0 +1,27 @@
+import { auth } from "../lucia"
+
+declare module "lucia" {
+    interface Register {
+        Lucia: typeof auth
+        DatabaseUserAttributes: {
+            avatar_url: string | null
+            banner_url: string | null
+            username: string
+            username_colour: string | null
+            email: string
+            email_verified: boolean
+            pronouns: string | null
+            verified: boolean
+            bio: string | null
+            date_joined: string
+            role_flags: number
+            is_contributor: boolean
+            self_assignable_role_flags: number | null
+        }
+        DatabaseSessionAttributes: {
+            user_agent: string
+            country_code: string
+            ip_address: string
+        }
+    }
+}
--- src/v2/lib/auth/lucia.ts
+++ src/v2/lib/auth/lucia.ts
@@ -1,72 +1,53 @@
-import { lucia } from "lucia"
-import { hono } from "lucia/middleware"
+import { Lucia } from "lucia"
 import { getConnection } from "@/v2/db/turso"
 import { tableNames } from "@/v2/db/drizzle"
-import { LuciaDBAdapter } from "./adapter/adapter"
-import { discord } from "@lucia-auth/oauth/providers"
+import { LibSQLAdapter } from "@lucia-auth/adapter-sqlite"
 
-/** The `auth` function is used to create a `lucia` instance with authentication middleware and a `libsql` adapter.
- * @param {Bindings} env - The environment variables used to configure the authentication middleware and adapter.
- * @returns A `lucia` instance with authentication middleware and a `libsql` adapter.
- */
 export function auth(env: Bindings) {
     const { turso } = getConnection(env)
 
-    return lucia({
-        adapter: LuciaDBAdapter(turso, {
+    // TODO(dromzeh): should probably utilize to the v3 drizzle adapter - it's okay for now though
+    return new Lucia(
+        new LibSQLAdapter(turso, {
             user: tableNames.authUser,
-            key: tableNames.authKey,
             session: tableNames.authSession,
         }),
-        middleware: hono(),
-        env: env.ENVIRONMENT === "DEV" ? "DEV" : "PROD",
-        experimental: {
-            debugMode: env.ENVIRONMENT === "DEV" ? true : false,
-        },
-        csrfProtection: {
-            enabled: true,
-            allowedSubDomains: ["*"],
-        },
-        getUserAttributes: (user) => {
-            return {
-                avatarUrl: user.avatar_url,
-                bannerUrl: user.banner_url,
-                username: user.username,
-                usernameColour: user.username_colour,
-                email: user.email,
-                emailVerified: user.email_verified,
-                pronouns: user.pronouns,
-                verified: user.verified,
-                bio: user.bio,
-                dateJoined: user.date_joined,
-                roleFlags: user.role_flags,
-                isContributor: user.is_contributor,
-                selfAssignableRoleFlags: user.self_assignable_role_flags,
-            }
-        },
-        getSessionAttributes: (session) => {
-            return {
-                userAgent: session.user_agent,
-                countryCode: session.country_code,
-                ipAddress: session.ip_address,
-            }
-        },
-    })
+        {
+            getUserAttributes: (user) => {
+                return {
+                    avatarUrl: user.avatar_url,
+                    bannerUrl: user.banner_url,
+                    username: user.username,
+                    usernameColour: user.username_colour,
+                    email: user.email,
+                    emailVerified: user.email_verified,
+                    pronouns: user.pronouns,
+                    verified: user.verified,
+                    bio: user.bio,
+                    dateJoined: user.date_joined,
+                    roleFlags: user.role_flags,
+                    isContributor: user.is_contributor,
+                    selfAssignableRoleFlags: user.self_assignable_role_flags,
+                }
+            },
+            getSessionAttributes: (session) => {
+                return {
+                    userAgent: session.user_agent,
+                    countryCode: session.country_code,
+                    ipAddress: session.ip_address,
+                }
+            },
+            sessionCookie: {
+                name: "user_auth_session",
+                expires: false,
+                attributes: {
+                    secure: env.ENVIRONMENT === "PROD",
+                    sameSite: env.ENVIRONMENT === "PROD" ? "strict" : "lax",
+                    path: "/",
+                },
+            },
+        }
+    )
 }
 
 export type Auth = ReturnType<typeof auth>
-
-/**
- * The `discordAuth` function is used to create a `discord` object for use with the `auth` function.
- * @param {Auth} auth - Auth instance used by Lucia.
- * @param {Bindings} env - Hono environment bindings.
- * @returns - Discord Object.
- */
-export function discordAuth(auth: Auth, env: Bindings) {
-    return discord(auth, {
-        clientId: env.DISCORD_CLIENT_ID,
-        clientSecret: env.DISCORD_CLIENT_SECRET,
-        redirectUri: env.DISCORD_REDIRECT_URI,
-        scope: ["identify", "email"],
-    })
-}
