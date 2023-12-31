4688364c0c8bcca84e37abda9ce78079140a691d
dromzeh <65048232+dromzeh@users.noreply.github.com>
2023-12-31 11:22:26 +0000

    update deps & rlly bad temp fix

--- src/v2/lib/auth/definitions/auth-definitions.ts
+++ src/v2/lib/auth/definitions/auth-definitions.ts
@@ -3,25 +3,25 @@ import type { LuciaAuth } from "../lucia"
 declare module "lucia" {
     interface Register {
         Lucia: LuciaAuth
-        DatabaseUserAttributes: {
-            avatar_url: string | null
-            banner_url: string | null
-            username: string
-            username_colour: string | null
-            email: string
-            email_verified: boolean
-            pronouns: string | null
-            verified: boolean
-            bio: string | null
-            date_joined: string
-            role_flags: number
-            is_contributor: boolean
-            self_assignable_role_flags: number | null
-        }
-        DatabaseSessionAttributes: {
-            user_agent: string
-            country_code: string
-            ip_address: string
-        }
+    }
+    interface DatabaseUserAttributes {
+        avatar_url: string | null
+        banner_url: string | null
+        username: string
+        username_colour: string | null
+        email: string
+        email_verified: boolean
+        pronouns: string | null
+        verified: boolean
+        bio: string | null
+        date_joined: string
+        role_flags: number
+        is_contributor: boolean
+        self_assignable_role_flags: number | null
+    }
+    interface DatabaseSessionAttributes {
+        user_agent: string
+        country_code: string
+        ip_address: string
     }
 }
