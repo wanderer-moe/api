ab889a1ac3138d146817e5766344cc899305b6a2
Marcel <65048232+dromzeh@users.noreply.github.com>
2024-01-28 02:24:51 +0000

    update deps

--- src/v2/lib/oslo.ts
+++ src/v2/lib/oslo.ts
@@ -1,4 +1,4 @@
-import { alphabet, generateRandomString } from "oslo/random"
+import { alphabet, generateRandomString } from "oslo/crypto"
 
 export function generateID(length: number = 15) {
     return generateRandomString(length, alphabet("a-z", "0-9")).toLowerCase()
