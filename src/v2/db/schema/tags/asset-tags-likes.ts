54c4f8b8948687f3102e2527b30bce383721f713
Marcel <65048232+dromzeh@users.noreply.github.com>
2024-03-29 05:11:34 +0000

    fix

--- src/v2/db/schema/tags/asset-tags-likes.ts
+++ src/v2/db/schema/tags/asset-tags-likes.ts
@@ -1,50 +1,50 @@
-import { tableNames } from "@/v2/db/drizzle"
-import { relations } from "drizzle-orm"
-import { index, sqliteTable, text } from "drizzle-orm/sqlite-core"
-import { authUser } from "../user/user"
-import { assetTag } from "./asset-tags"
-import { createInsertSchema, createSelectSchema } from "drizzle-zod"
-export const assetTagLikes = sqliteTable(
-    tableNames.assetTagLikes,
-    {
-        assetTagId: text("asset_id")
-            .notNull()
-            .references(() => assetTag.id),
-        likedById: text("liked_by_id")
-            .notNull()
-            .references(() => authUser.id),
-        createdAt: text("created_at")
-            .notNull()
-            .$defaultFn(() => {
-                return new Date().toISOString()
-            }),
-    },
-    (gameLikes) => {
-        return {
-            assetTagIdx: index("assetTagLikes_asset_idx").on(
-                gameLikes.assetTagId
-            ),
-            likedByIdx: index("assetTagLikes_likedby_idx").on(
-                gameLikes.likedById
-            ),
-        }
-    }
-)
+// import { tableNames } from "@/v2/db/drizzle"
+// import { relations } from "drizzle-orm"
+// import { index, sqliteTable, text } from "drizzle-orm/sqlite-core"
+// import { authUser } from "../user/user"
+// import { assetTag } from "./asset-tags"
+// import { createInsertSchema, createSelectSchema } from "drizzle-zod"
+// export const assetTagLikes = sqliteTable(
+//     tableNames.assetTagLikes,
+//     {
+//         assetTagId: text("asset_id")
+//             .notNull()
+//             .references(() => assetTag.id),
+//         likedById: text("liked_by_id")
+//             .notNull()
+//             .references(() => authUser.id),
+//         createdAt: text("created_at")
+//             .notNull()
+//             .$defaultFn(() => {
+//                 return new Date().toISOString()
+//             }),
+//     },
+//     (gameLikes) => {
+//         return {
+//             assetTagIdx: index("assetTagLikes_asset_idx").on(
+//                 gameLikes.assetTagId
+//             ),
+//             likedByIdx: index("assetTagLikes_likedby_idx").on(
+//                 gameLikes.likedById
+//             ),
+//         }
+//     }
+// )
 
-export type AssetTagLikes = typeof assetTagLikes.$inferSelect
-export type NewAssetTagLikes = typeof assetTagLikes.$inferInsert
-export const insertAssetTagLikesSchema = createInsertSchema(assetTagLikes)
-export const selectAssetTagLikesSchema = createSelectSchema(assetTagLikes)
+// export type AssetTagLikes = typeof assetTagLikes.$inferSelect
+// export type NewAssetTagLikes = typeof assetTagLikes.$inferInsert
+// export const insertAssetTagLikesSchema = createInsertSchema(assetTagLikes)
+// export const selectAssetTagLikesSchema = createSelectSchema(assetTagLikes)
 
-export const assetTagLikesRelations = relations(assetTagLikes, ({ one }) => ({
-    assetTag: one(assetTag, {
-        fields: [assetTagLikes.assetTagId],
-        references: [assetTag.id],
-        relationName: "assetTagLikes_liked_assetTag",
-    }),
-    likedBy: one(authUser, {
-        fields: [assetTagLikes.likedById],
-        references: [authUser.id],
-        relationName: "assetTagLikes_liked_by",
-    }),
-}))
+// export const assetTagLikesRelations = relations(assetTagLikes, ({ one }) => ({
+//     assetTag: one(assetTag, {
+//         fields: [assetTagLikes.assetTagId],
+//         references: [assetTag.id],
+//         relationName: "assetTagLikes_liked_assetTag",
+//     }),
+//     likedBy: one(authUser, {
+//         fields: [assetTagLikes.likedById],
+//         references: [authUser.id],
+//         relationName: "assetTagLikes_liked_by",
+//     }),
+// }))
