014fa58157360491ab4a6c20d784bc0a775701fc
Marcel <65048232+dromzeh@users.noreply.github.com>
2024-04-04 22:59:01 +0100

    ?

--- src/v2/routes/auth/account-login.ts
+++ src/v2/routes/auth/account-login.ts
@@ -14,10 +14,6 @@ const requestBodySchema = z.object({
         description: "The password of the user.",
         example: "password1234",
     }),
-    passwordConfirmation: z.string().min(8).max(64).openapi({
-        description: "The password confirmation of the user.",
-        example: "password1234",
-    }),
 })
 
 const responseSchema = z.object({
