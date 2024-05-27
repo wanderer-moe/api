e973363e672559999ecb55146b7499e711cd6742
Marcel <65048232+dromzeh@users.noreply.github.com>
2024-05-28 00:36:12 +0100

    opentelemetry (plz dont break prod)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import { router } from "@/handler";
+import { instrument, ResolveConfigFn } from "@microlabs/otel-cf-workers";
 
-export default {
+const handler = {
     fetch: router.handle,
 };
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const config: ResolveConfigFn = (env: Env, _trigger) => {
+    return {
+        exporter: {
+            url: "https://otel.baselime.io/v1",
+            headers: { "x-api-key": env.BASELIME_API_KEY },
+        },
+        service: { name: env.SERVICE_NAME },
+    };
+};
+
+export default instrument(handler, config);
--- src/worker-configuration.d.ts
+++ src/worker-configuration.d.ts
@@ -3,4 +3,6 @@ interface Env {
     bucket: R2Bucket;
     database: D1Database;
     DISCORD_LOG_WEBHOOK_URL: string;
+    BASELIME_API_KEY: string;
+    SERVICE_NAME: string;
 }
